37dbf018650eb9e3ab12d8c2ff11041767293dab
= <=>
2022-08-21 13:54:33 +0200

    change credentials

--- apps/emotes/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import { Environment } from '@nxt/environment';
-
-export const environment: Environment = {
-  production: false,
-  twitchClientId: 'qz530ybtrsnx39qp9n930ppctsb6to',
-  twitchClientSecret: 'hvkfz5rd4dq16at9j7g0brpmaxk8u4',
-};
